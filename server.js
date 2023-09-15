55e89849f7b2495df75878463ec2c8fbf47036b9
itzelmeritxell <itzelmeritxell@gmail.com>
2023-09-15 18:15:35 -0400

    updating file

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
 
+
 app.get('/', (req, res) => {
     res.send('Itzel Velez - 179425210');
-});
+  });
 
 // start the server on the port and output a confirmation ot the console
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
